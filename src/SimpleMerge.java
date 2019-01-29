582101dc838f27e1c4011a1deb824619420f3ec8
KarwaiKang <kkang9093@bths.edu>
2019-01-29 11:37:30 -0500

    Add simpleMerge method in SimpleMerge class

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,7 +6,14 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
+        int[] out = new int[arr1.length + arr2.length];
+        for (int i = 0, idx1 = 0, idx2 = 0; i < out.length; i++) {
+            if (idx2 == arr2.length || idx1 < arr1.length && arr1[idx1] < arr2[idx2])
+                out[i] = arr1[idx1++];
+            else
+                out[i] = arr2[idx2++];
+        }
+        return out;
     }
 
 }
